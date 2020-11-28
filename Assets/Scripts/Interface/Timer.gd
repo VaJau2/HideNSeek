0e60608c05e2d85d8e03ffb650e5b63fa4294831
vaja72 <vadim206-1998@yandex.ru>
2020-11-28 19:19:58 +0500

    timer counting label

---
+++ Assets/Scripts/Interface/Timer.gd
@@ -0,0 +1,44 @@
+extends Label
+
+var minutes = 0
+var seconds = 0
+
+signal timeout
+
+
+func _makeWork(work: bool) -> void:
+	visible = work
+	set_process(work)
+
+
+func _getSecondString() -> String:
+	var tempSeconds = round(seconds)
+	var strSeconds = str(tempSeconds)
+	if tempSeconds < 10:
+		strSeconds = "0" + strSeconds
+	return strSeconds
+	
+
+func StartTimer(_seconds, _minutes = 0) -> void:
+	minutes = _minutes
+	seconds = _seconds
+	_makeWork(true)
+
+
+func _ready():
+	G.timer = self
+	_makeWork(false)
+
+
+func _process(delta):
+	text = str(minutes) + ":" + _getSecondString()
+	
+	if seconds > 0:
+		seconds -= delta
+	else:
+		if minutes > 0:
+			minutes -= 1
+			seconds = 60
+		else:
+			emit_signal("timeout")
+			_makeWork(false)
--- Assets/Scripts/global.gd
+++ Assets/Scripts/global.gd
@@ -5,3 +5,4 @@ enum GAME_STATE {IDLE, HIDING, SEARCHING}
 var player: Character
 var dialogueMenu
 var state = GAME_STATE.IDLE
+var timer
